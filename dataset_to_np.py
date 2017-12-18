c102942d330937121fd1f6b41b4b511928eadf4a
Dario Sitnik <dario.sitnik@gmail.com>
2017-12-18 19:23:56 +0100

    dataset to numpy files

---
+++ dataset_to_np.py
@@ -0,0 +1,21 @@
+import numpy as np 
+import os
+import cv2
+
+imgs = []
+for i, file in enumerate(os.listdir()):
+    if i%100 == 0:
+        print (i)
+    if file == "ssims.txt":
+        f = open("ssims.txt", "r")
+        ssims = f.readlines()
+        np.save("Y_data", np.asarray(ssims).astype(np.float64))
+        continue
+    if file == "dataset_to_np.py":
+        continue
+    imgs.append(cv2.imread(file,0))
+np.save("X_data", np.asarray(imgs))
+
+
+
+    
