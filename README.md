9b6e9a3256eb9d672d4b188ee8b90f5ad68a9d5e
Matej <matej.crnac@gmail.com>
2017-12-26 22:09:20 +0100

    Changed readme

--- README.md
+++ README.md
@@ -5,5 +5,17 @@ Project is written in python 3.6
 
 Database links are:
 Caltech 101: http://www.vision.caltech.edu/Image_Datasets/Caltech101/
+    Direct download link: http://www.vision.caltech.edu/Image_Datasets/Caltech101/101_ObjectCategories.tar.gz
 
 MDID (MDID is a database for evaluating the results of image quality assessment metrics on multiply distorted images):  http://www.sz.tsinghua.edu.cn/labs/vipl/mdid.html
+
+
+FILES:
+
+dataset_maker.py
+datasetPreparation.py
+dataset_to_np.py
+deep_nn.py
+program.py
+README
+SSIM_calculator.ipynb
--- program.py
+++ program.py
@@ -24,84 +24,84 @@ from sklearn.model_selection import train_test_split
 
 
 class NeuralNetwork:
-	def __init__(self):
-		self.model=VGG16()
-		return
-	
+    def __init__(self):
+        self.model=VGG16()
+        return
 
-	#as input takes list of images and path to output file(y-results)
-	def train(self,X,Y,normalizeInput=0,batch=64):
-		self.num=len(X)
-		if(normalizeInput):
-			meanImage,sumImage = getMeanImage(X)
-			self.meanImage=meanImage
-			self.sumImage=sumImage
-		for x,y in zip(X,Y):
-			if(normalizeInput):
-				x-=meanImage
-			#Kako se salju ulazi? kao lista ili na vertikalni numpy??
-			self.model.fit(x, y, epochs=100, batch_size=batch,  verbose=1)  #nije mi jasno da li je ovo online ucenje
-		return
 
-	def classify(self,image):
-		return self.model.predict(image)
+    #as input takes list of images and path to output file(y-results)
+    def train(self,X,Y,normalizeInput=0,batch=64):
+        self.num=len(X)
+        if(normalizeInput):
+            meanImage,sumImage = getMeanImage(X)
+            self.meanImage=meanImage
+            self.sumImage=sumImage
+        for x,y in zip(X,Y):
+            if(normalizeInput):
+                x-=meanImage
+            #Kako se salju ulazi? kao lista ili na vertikalni numpy??
+            self.model.fit(x, y, epochs=100, batch_size=batch,  verbose=1)  #nije mi jasno da li je ovo online ucenje
+        return
+
+    def classify(self,image):
+        return self.model.predict(image)
 
 
 """
 Function as an input gets a list of names(paths), loads them, calculates mean image in a folder and return mean and sum image
 """
 def getMeanImage(X):
-	num=len(X)
-	sumImage = np.zeros(X[0].shape)
-	for i,pic in enumerate(X):
-		sumImage = np.add(sumImage,pic)
-		if i%500==0:
-			print(i)
-	#print (sumImage)
-	meanImage=sumImage/num
-	#print (meanImage)
-	return meanImage,sumImage
+    num=len(X)
+    sumImage = np.zeros(X[0].shape)
+    for i,pic in enumerate(X):
+        sumImage = np.add(sumImage,pic)
+        if i%500==0:
+            print(i)
+    #print (sumImage)
+    meanImage=sumImage/num
+    #print (meanImage)
+    return meanImage,sumImage
 
 
 def getFiles(root):
-	print(root)
-	listOfNames=[]
-	for path, subdirs, files in os.walk(root):
-		#print subdirs
-		#print(files)
-		for name in files:
-			if(len(name.split("."))>1):
-				if(name.split(".")[1]=="jpg"):
-					#print(os.path.join(path, name))
-					listOfNames.append(os.path.join(path, name))
-					#print (os.path.join(path, name))
-	#print (listOfNames)
-	Collection = imread_collection(listOfNames)
-	#imshow(Collection[0])
-	#imshow(random_noise(Collection[0]))
-	return listOfNames,Collection
-	pass  
+    print(root)
+    listOfNames=[]
+    for path, subdirs, files in os.walk(root):
+        #print subdirs
+        #print(files)
+        for name in files:
+            if(len(name.split("."))>1):
+                if(name.split(".")[1]=="jpg"):
+                    #print(os.path.join(path, name))
+                    listOfNames.append(os.path.join(path, name))
+                    #print (os.path.join(path, name))
+    #print (listOfNames)
+    Collection = imread_collection(listOfNames)
+    #imshow(Collection[0])
+    #imshow(random_noise(Collection[0]))
+    return listOfNames,Collection
+    pass
 
 
 if __name__ == "__main__":
-	#root = "D:\FER\Neural nets\\101_ObjectCategories\\accordion"
-	#root = os.getcwd()+"/output"
-	#rootOutput = os.getcwd()+"/output/ssims.txt"
-	#listOfDirs = getFiles(root)
-	#listOfPics = listOfDirs[0]
-	X = np.load("X_data.npy")
-	Y = np.load("Y_data.npy")
-	#meanImage,sumImage = getMeanImage(listOfPics)
-	"""	
-	num=len(listOfDirs)
-	sumImage = np.zeros(imread(listOfDirs[0]).shape)
-	for i,name in enumerate(listOfDirs):
-		sumImage = np.add(sumImage,imread(name))
-		if i%500==0:
-			print(i)
-	print (sumImage)
-	meanImage=sumImage/num
-	print (meanImage)
-	"""
-	#X_train5, X_test5, y_train5, y_test5 = train_test_split(X, Y, test_size=0.33, random_state=42)
+    #root = "D:\FER\Neural nets\\101_ObjectCategories\\accordion"
+    #root = os.getcwd()+"/output"
+    #rootOutput = os.getcwd()+"/output/ssims.txt"
+    #listOfDirs = getFiles(root)
+    #listOfPics = listOfDirs[0]
+    X = np.load("X_data.npy")
+    Y = np.load("Y_data.npy")
+    #meanImage,sumImage = getMeanImage(listOfPics)
+    """
+    num=len(listOfDirs)
+    sumImage = np.zeros(imread(listOfDirs[0]).shape)
+    for i,name in enumerate(listOfDirs):
+        sumImage = np.add(sumImage,imread(name))
+        if i%500==0:
+            print(i)
+    print (sumImage)
+    meanImage=sumImage/num
+    print (meanImage)
+    """
+    #X_train5, X_test5, y_train5, y_test5 = train_test_split(X, Y, test_size=0.33, random_state=42)
 
